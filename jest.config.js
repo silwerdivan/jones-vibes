a96d95b211a84e447bf0c9689105dce5afc49804
Divan Jacobs <silwerdivan@gmail.com>
2025-10-07 06:21:47 +0200

    chore(testing): configure jest for es modules and dom testing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     testEnvironment: 'jest-environment-jsdom',
     transform: {
-        '^.+\.js$': 'babel-jest',
+        '^.+\\.js$': 'babel-jest',
     },
+    setupFilesAfterEnv: ['./jest.setup.js'],
+    moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
 };
