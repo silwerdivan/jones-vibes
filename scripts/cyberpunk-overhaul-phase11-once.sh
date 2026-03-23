9762e1e0358714d2f3c5047e6ed755c33872e283
Divan Jacobs <silwerdivan@gmail.com>
2026-03-23 05:27:03 +0200

    Add empty message argument for opencode run

--- scripts/cyberpunk-overhaul-phase11-once.sh
+++ scripts/cyberpunk-overhaul-phase11-once.sh
@@ -974,7 +974,7 @@ case "${AGENT_EXEC}" in
     codex_args=(
       bash
       -c
-      "opencode run --project '${ROOT_DIR}' --file '${SLICE_PROMPT_FILE}'"
+      "opencode run '' --project '${ROOT_DIR}' --file '${SLICE_PROMPT_FILE}'"
     )
     ;;
   *)
